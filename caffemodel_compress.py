a6b0f568c9677bb0c35d47c32ca7b97b35aecaf2
sktzwhj <whj_nudt@foxmail.com>
2017-03-23 22:12:58 +1100

    understand how to use it

--- caffemodel_compress.py
+++ caffemodel_compress.py
@@ -4,9 +4,14 @@ import numpy as np
 from math import ceil
 #from quantz_kit.weights_quantization import weights_quantization
 import sys
-sys.path.append("./quantz_kit")
+sys.path.append("/usr/lib/python2.7/dist-packages/quantz_kit")
 import weights_quantization as wqtz
 import time
+
+
+
+#we just need to understand that in order to compute the accuracy and loss of the compressed model, you still have
+#to decode the model because it is not implemented in caffe. Caffe cannot organize the memory for compressed model.
 
 
 def caffe_model_compress(model, weights, storefile, convbit=6, fcbit=2, use_savez_compressed=True):
@@ -16,6 +21,8 @@ def caffe_model_compress(model, weights, storefile, convbit=6, fcbit=2, use_save
 	xdict = dict()
 	#version 1 ; bits of conv layer and bits of full-connected layer
 	xdict['compz_info'] = (1, int(convbit), int(fcbit))
+
+	ret = None
 	
 	for item in net.params.items():
 		name, layer = item
@@ -33,6 +40,11 @@ def caffe_model_compress(model, weights, storefile, convbit=6, fcbit=2, use_save
 			nbit = int(convbit)
 
 		weights_vec = weights.flatten().astype(np.float32)
+
+
+
+		ret = weights
+
 		vec_length = weights_vec.size
 		nelem = 32 / nbit
 		newlabel = np.empty(((vec_length+nelem-1)/nelem),dtype=np.int32) 
@@ -44,17 +56,24 @@ def caffe_model_compress(model, weights, storefile, convbit=6, fcbit=2, use_save
 		#kmeans_time = kmeans_time + t_stop - t_start
 				
 		xdict[name+'_weight_labels'] = newlabel
+		xdict[name+'_weight_labels'] = newlabel
 		xdict[name+'_weight_codebook'] = codebook
 		xdict[name+'_bias'] = bais
 
 	#keep result into output file
+
+	print 'writing the result to file %s'%storefile
 	if (use_savez_compressed):
 		np.savez_compressed(storefile, **xdict)
 	else:
 		np.savez(storefile, **xdict)
+	return ret
 
 	
 def caffe_model_decompress(model, weights, loadfile):
+
+	print 'loadfile',loadfile
+	print 'weightfile',weights
 	net = caffe.Net(model, caffe.TEST);
 	cmpr_model = np.load(loadfile)
 	
@@ -63,12 +82,13 @@ def caffe_model_decompress(model, weights, loadfile):
 	version, convbit, fcbit = cmpr_model['compz_info']
 	
 	assert(version == 1), "compz version not support"
-	
+	ret2= None
 	
 	for item in net.params.items():
 		name, layer = item
 		newlabels = cmpr_model[name+'_weight_labels']
 		codebook = cmpr_model[name+'_weight_codebook']
+		print len(codebook)
 
 		origin_size = net.params[name][0].data.flatten().size
 		weights_vec = np.empty(origin_size, dtype=np.float32)
@@ -80,21 +100,40 @@ def caffe_model_decompress(model, weights, loadfile):
 		elif "conv" in name:
 			nbit = convbit
 
+
 		wqtz.decompress_layer_weights(weights_vec, newlabels, codebook, vec_length, nbit)
 		newweights = weights_vec.reshape(net.params[name][0].data.shape)
+		ret2= newweights
 		net.params[name][0].data[...] = newweights
 
 		newbias = cmpr_model[name+'_bias']
 		net.params[name][1].data[...] = newbias[...]
+
 	net.save(weights)
+	return ret2
+
+
 
 if __name__ == "__main__":
-	ALEXNET_PATH = "/home/junyi/caffe/models/bvlc_alexnet"
-	netmodel   = os.path.join(ALEXNET_PATH, "deploy.prototxt") 
-	netweights = os.path.join(ALEXNET_PATH, "bvlc_alexnet.caffemodel")
-	output = "alexnetzip"
-	caffe_model_compress(netmodel, netweights, output, 6, 2)
+	#ALEXNET_PATH = "/home/junyi/caffe/models/bvlc_alexnet"
+	#ALEXNET_PATH = sys.argv[1]
+	ALEXNET_PATH = "/home/wuhuijun/caffe/examples/mnist/"
+	#netmodel   = os.path.join(ALEXNET_PATH, "deploy.prototxt")
+	#netmodel = os.path.join(ALEXNET_PATH, sys.argv[2])
+	netmodel = os.path.join(ALEXNET_PATH, "lenet_train_test.prototxt")
+	#netweights = os.path.join(ALEXNET_PATH, "bvlc_alexnet.caffemodel")
+	#netweights = os.path.join(ALEXNET_PATH, sys.argv[3])
+	netweights = os.path.join(ALEXNET_PATH, "lenet_iter_10000.caffemodel")
 
-	caffe_model_decompress(netmodel, "alexnet_xx.caffemodel", "alexnetzip.npz")
-	
+	output = "lenet_compressed"
+
+	ret1 = caffe_model_compress(netmodel, netweights, output, 9, 2)
+	#print 'before compression, the weight vector is ', ret1
+	ret2=caffe_model_decompress(netmodel, "lenet_xx", "lenet_compressed.npz")
+	#print 'after compression, the weight vector is ', weights_vec
+	#print np.array(ret1[0])
+	#print (np.array(ret1) - np.array(ret2))/np.array(ret1)
+	#print np.array(ret2[0])
+	#print len(set(ret2.flatten()))
 	print "Done"
+
